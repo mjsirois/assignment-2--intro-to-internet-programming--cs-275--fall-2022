6f1df2bc560521f2a54818b74389764bc7c20842
Matthew Sirois <mjsirois2@gmail.com>
2022-10-27 11:06:37 -0400

    Configuring images/buttons, aligning

---
+++ js/app.js
@@ -0,0 +1,11 @@
+window.onload = () => {
+    let rightButton = document.getElementById(`rightArrow`);
+    let leftButton = document.getElementById(`leftArrow`);
+
+    let img = document.getElementsByClassName(`img-black`);
+    let img2 = document.getElementsByClassName(`img-red`);
+    let img3 = document.getElementsByClassName(`img-blue`);
+    let img4 = document.getElementsByClassName(`img-original`);
+    let img5 = document.getElementsByClassName(`img-green`);
+
+}
